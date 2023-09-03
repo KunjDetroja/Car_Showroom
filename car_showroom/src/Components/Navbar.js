26b283ecf5fe4e2016da320be6d13732232777a6
ghost3344 <kashyapjivani@gmail.com>
2023-09-03 14:44:34 +0530

    fix issue

--- car_showroom/src/Components/Navbar.js
+++ car_showroom/src/Components/Navbar.js
@@ -10,35 +10,27 @@ import { Row,Col } from "react-bootstrap";
 function NavScrollExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid className="mainnav">
-        <Row>
-          <Col lg={12} md={12} xl={12}>
-          <Navbar.Brand href="#">CarJovo</Navbar.Brand>
-     
-          </Col>
-          <Col lg={12} md={12} xl={12}>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <div></div>
-          <Form className='d-flex'>
-            <Form.Control
-              type="search"
-              placeholder="Search Car"
-              // className="me-2"
-              aria-label="Search"
-            />
-            <Button className="searchbutton"><BiSearch/></Button>
-          </Form>
-        
-        <div>
-          <AiOutlineUser className="usericon" />
-          <span className="login">Login / Register</span>
-        </div></Navbar.Collapse>
-          </Col>
-        </Row>
-  
-      </Container>
-    </Navbar>
+    <Container fluid className="mainnav">
+      <Navbar.Brand href="#">CarJovo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <div></div>
+        <Form className='d-flex'>
+          <Form.Control
+            type="search"
+            placeholder="Search Car"
+            // className="me-2"
+            aria-label="Search"
+          />
+          <Button className="searchbutton"><BiSearch/></Button>
+        </Form>
+      
+      <div>
+        <AiOutlineUser className="usericon" />
+        <span className="login">Login / Register</span>
+      </div></Navbar.Collapse>
+    </Container>
+  </Navbar>
     
   );
 }
