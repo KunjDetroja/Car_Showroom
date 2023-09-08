7b6c93bcba5afe01791c392262169f9eaaf613ab
Kunj <kunjdetroja52@gmail.com>
2023-09-08 23:12:50 +0530

    Solve issue

--- car_showroom/src/Components/Home.jsx
+++ car_showroom/src/Components/Home.jsx
@@ -101,68 +101,6 @@ function Home() {
           <div className="col-2"><BiMap className="usericon"/>
               <span>Select City</span></div>
         </div>
-        {/* <nav className="navbar navbar-expand-lg navbar-light ">
-          <div className="container-fluid mainnav2">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link
-                    className="nav-link active"
-                    aria-current="page"
-                    to="/buy"
-                  >
-                    Buy Car
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="#">
-                    Used Car
-                  </Link>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link
-                    className="nav-link dropdown-toggle"
-                    to="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Area
-                  </Link>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    {area.map((x) => (
-                      <li>
-                        <Link className="dropdown-item" to="#">
-                          {x}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              </ul>
-              <BiMap/>
-              <span>Select City</span>
-            </div>
-          </div>
-        </nav> */}
         <Routes>
           <Route exact path="/" element={<Carhome />}></Route>
           <Route exact path="/buy" element={<Buy />}></Route>
